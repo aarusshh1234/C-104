63f84e3be2b0142abfe0c5516c0db5da2a812e9b
aarusshh1234 <77450793+aarusshh1234@users.noreply.github.com>
2023-04-04 16:09:15 +0530

    Add files via upload

---
+++ imageWithArrays.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+
+black = np.zeros([600,600])
+
+
+
+black [400:500, 100:300 ] = 255
+print(black)
+cv2.imshow("black", black)
+cv2.waitKey(0)
---
+++ readImage.py
@@ -0,0 +1,13 @@
+import cv2
+
+img = cv2.imread('butterfly.jpg')
+
+cv2.imshow("display image", img)
+
+#print(img)
+
+grey_img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+cv2.imshow("Grey image", grey_img)
+print(grey_img)
+cv2.waitKey(0)
+
